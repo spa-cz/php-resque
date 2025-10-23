2c1b066989b3fac4f6f3aaf4d6dd72fdd8960ad5
jiri.michel <jiri.michel@hotel.cz>
2025-10-23 16:28:30 +0200

    Rector-ing

---
+++ rector.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+// NOTE: When editing this file, make sure to order line groups alphabetically (so git can diff it nicely)
+// use statements, and each group inside withRules()
+
+// PHP upgrades
+use Rector\Php82\Rector\Encapsed\VariableInStringInterpolationFixerRector;
+use Rector\Php84\Rector\Param\ExplicitNullableParamTypeRector;
+
+// Code Quality / Style
+use Rector\CodingStyle\Rector\FuncCall\ConsistentImplodeRector;
+
+// Migrate rules one at a time
+$rules = [
+    
+    // PHP 8.2
+    VariableInStringInterpolationFixerRector::class,
+
+    // PHP 8.4
+    ExplicitNullableParamTypeRector::class,
+
+    // Code Quality / Style
+    ConsistentImplodeRector::class,
+];
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/lib/Resque',
+    ])
+    /* ->withFileExtensions([
+        'php',
+        'phtml',
+    ]) */
+    ->withPhpVersion(\Rector\ValueObject\PhpVersion::PHP_84)
+    /* ->withPreparedSets(
+        deadCode: true,
+    ) */
+    /* ->withPhpSets(
+        php84: true,
+    ) */
+    ->withRules($rules);
